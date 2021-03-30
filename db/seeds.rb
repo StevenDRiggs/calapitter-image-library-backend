5612dc606df1f96ac0cc522e14549f6762f83280
StevenDRiggs <sfriggs21@gmail.com>
2021-03-29 21:55:44 -0600

    initial revamp commit; re-created rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
-require 'faker'
-
-
-15.times do
-  Image.create(url: "https://picsum.photos/#{(100..300).to_a.sample}")
-end
-
-3.times do
-  User.create(username: Faker::Name.first_name + (Random.rand * 100).floor.to_s, avatar: "https://picsum.photos/100", password: 'password')
-end
-
-for i in (1..((Random.rand * 3).floor + 1)) do
-  u = User.find(i)
-  images = []
-  (Random.rand * 5).floor.times do
-    u.images << Image.all.sample
-  end
-  u.save
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
