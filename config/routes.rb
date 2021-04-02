b2d2a2ed2a4354eb1e3ae99d54a4c7e724471a0c
StevenDRiggs <sfriggs21@gmail.com>
2021-04-02 14:43:44 -0600

    began adding logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
   post '/login', to: 'users#login'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/logout', to: 'users#logout'
 end
